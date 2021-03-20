0e004ca3bcd8e35dfe6de35329ff3962566eeae4
Alberto <alberto.garcia56@alu.uclm.es>
2021-03-20 17:21:26 +0100

    Subida_final

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "/notes", to: "notes#index"
-
+  root "notes#index"
+  
+  resources :notes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
