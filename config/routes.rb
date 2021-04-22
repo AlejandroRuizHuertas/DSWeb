b2fe7339a5ae4a437ecf137b6e9376b6600aaff7
Alberto <garciamarquezalberto9@gmail.com>
2021-04-22 16:05:38 +0200

    Fallo tonto

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
   root "sessions#login"
-
-  resources :users
+  
   resources :notes
 
   post "/" => "sessions#create"
   get "logout" => "sessions#destroy"
-  get "users/register" => "users#register"
+  get "users/register" => "users#new"
   post "users/register" => "users#create"
   get "notes/:id/show" => "notes#show", as: :show
   get "notes/new" => "notes#register"
   post "notes/new" => "notes#create"
   get "admin/management" => "admin#management"
+
+  resources :users
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
